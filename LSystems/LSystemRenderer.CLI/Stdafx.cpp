3172e1b63d62fedadbfd6c5133f533cca6bfdd22
nlugic <nlugic@gmail.com>
2018-08-16 23:53:14 +0200

    reverting to GLFW

--- LSystems/LSystemRenderer.CLI/Stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// LSystemRenderer.CLI.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
--- LSystems/LSystemRenderer/LSystemRenderer.h
+++ LSystems/LSystemRenderer/LSystemRenderer.h
@@ -1,19 +1,13 @@
 #ifndef LSYSTEMRENDERER_H
 #define LSYSTEMRENDERER_H
 
-#ifdef LSYSTEMRENDERER_EXPORTS
-#define LSYSTEMRENDERER_API __declspec(dllexport)
-#else
-#define LSYSTEMRENDERER_API __declspec(dllimport)
-#endif
-
 namespace LSystemRendererAPI
 {
 
 	class LSystemRenderer
 	{
 	public:
-		static LSYSTEMRENDERER_API void test();
+		void test();
 	};
 
 }
